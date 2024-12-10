e4742e97cafe67853f30d5d517efe4247ce8792a
Brenda (Siyue) Qiu <siyueq@uw.edu>
2024-12-10 14:00:52 -0800

    removed add_time_to_sample; updated demo to include usage of define_aoi

--- visualeyes/core/utility.py
+++ visualeyes/core/utility.py
@@ -2,46 +2,6 @@ import eyelinkio
 import pandas as pd
 import numpy as np
 
-def add_time_to_sample(edf, edf_dfs):
-    
-    '''
-    Add time to the "sample" dataframe
-    
-    Parameters:
-    -----------
-    edf_dfs : dict
-        Dictionary of pandas dataframes from eyelinkio.EDF.to_pandas() output
-    edf: eyelinkio.edf.EDF
-        eyelinkio.edf.EDF object
-        
-    Returns:
-    --------
-    edf_dfs : dict
-        Dictionary of pandas dataframes from eyelinkio.EDF.to_pandas() output with 
-        time column added to the "sample" dataframe
-    '''
-    
-    # check if edf_dfs is a dictionary of dataframes
-    if not isinstance(edf_dfs, dict):
-        raise ValueError('edf_dfs should be a dictionary of dataframes')
-    
-    # check if sample dataframe is in the dictionary
-    if 'samples' not in edf_dfs.keys():
-        raise ValueError('samples dataframe is not in the dictionary')
-    
-    # check if edf is an eyelinkio.edf.EDF object
-    if not isinstance(edf, eyelinkio.edf.EDF):
-        raise ValueError('edf should be an eyelinkio.edf.EDF object')
-    
-    # check if edf contains time column
-    if 'times' not in edf.keys():
-        raise ValueError('data should contain a time column')
-    
-    # add time to the "sample" dataframe
-    edf_dfs['samples']['time'] = edf['times']
-    
-    return edf_dfs
-
 def aoi_mask_validation(aoi_mask, screen_dimension):
     
     '''
@@ -108,11 +68,12 @@ def dataframe_validation(df):
         raise ValueError('missing x and y coordinates')
     
     else:
+        # pull the x and y coordinates
         if 'axp' in df.columns and 'ayp' in df.columns:
             x_coord = df['axp']
             y_coord = df['ayp']
         else:
             x_coord = df['xpos']
             y_coord = df['ypos']
-
+            
     return (x_coord.values, y_coord.values)
