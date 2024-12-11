4125f44cfeb2a7d825add020f647b1925cd58347
S. Bahar Sener <111606281+baharsener@users.noreply.github.com>
2024-12-11 13:18:02 -0800

    fixed typo

--- visualeyes/core/__init__.py
+++ visualeyes/core/__init__.py
@@ -1,3 +1,3 @@
 from .processing import define_aoi, epoch_data, percent_data_in_aoi
 from .utility import add_time_to_sample, aoi_mask_validation, dataframe_validation
-from .plotting import plot_as_scatter, overlay_aoi, plot_heatmap
+from .plotting import plot_as_scatter, aoi_overlay, plot_heatmap
