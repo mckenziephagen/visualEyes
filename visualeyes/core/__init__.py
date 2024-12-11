1a55dce0e9e58fc68e968d5d00163bba992685d1
Lydia Zhang <yzh22@uw.edu>
2024-12-10 16:36:28 -0800

    resolve conflicts

--- visualeyes/core/__init__.py
+++ visualeyes/core/__init__.py
@@ -1,2 +1,3 @@
 from .processing import define_aoi, epoch_data, percent_data_in_aoi
-from .plotting import plot_fixations_aoi
+from .utility import aoi_mask_validation, dataframe_validation
+from .plotting import plot_as_scatter
