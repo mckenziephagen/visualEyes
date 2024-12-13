53d04d8afd04baff7cc96170a1c522c314d82f5e
mckenziephagen <mphagen@uw.edu>
2024-12-12 20:26:04 -0800

    fix import typo

--- visualeyes/core/__init__.py
+++ visualeyes/core/__init__.py
@@ -1,3 +1,3 @@
 from .processing import define_aoi, epoch_data, percent_data_in_aoi
 from .utility import aoi_mask_validation, dataframe_validation
-from .plotting import plot_as_scatter, overylay_aoi, plot_heatmap
+from .plotting import plot_as_scatter, overlay_aoi, plot_heatmap
